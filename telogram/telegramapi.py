a07966abdd8cacd6a2c28d5fd0df4a1071f3ba67
Jaume Serrats <jaume.serrats9@gmail.com>
2019-10-26 16:59:07 +0200

    bug fix

--- telogram/telegramapi.py
+++ telogram/telegramapi.py
@@ -7,5 +7,5 @@ def send(id, token, text):
     data = {'chat_id': id, 'text': text}
     path = "/bot" +token + "/sendMessage"
     connection.request('POST', path, json.dumps(data), headers)
-    #response = connection.getresponse()
+    response = connection.getresponse()
     #print(response.read().decode())
